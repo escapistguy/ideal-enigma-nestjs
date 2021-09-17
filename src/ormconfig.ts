29f29ad006ad7ee59a4d0bf33810318e70bd2a18
Diego Armando Arana Preciado <darana@Diegos-MacBook-Air.local>
2021-09-17 15:17:38 -0500

    added migrations support

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -8,7 +8,11 @@ const config: ConnectionOptions = {
     password: "welcome123",
     database: "mediumclone",
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: true
+    synchronize: false,
+    migrations: [__dirname + "/migrations/**/*{.ts,.js}"],
+    cli: {
+        migrationsDir: __dirname + "/migrations"
+    }
 };
 
 export default config;
